9f2acaf1d412dca6406db6af878589a51cd4577c
FilipSokol <sok.filip@gmail.com>
2021-04-17 11:56:58 +0200

    Optimization of convertTemperature function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Calculator from './components/Calculator';
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,76 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-    const TemperatureField = ({temperature, scale, onTemperatureChange}) => {
-        return(
-            <input 
-            value = {temperature}
-            type = "number" 
-            scale = {scale}
-            onChange = {(e) => onTemperatureChange(e.target.value)}
-            />
-        );
-    }
+const TemperatureField = ({ temperature, scale, onTemperatureChange }) => {
+  return (
+    <input
+      value={temperature}
+      type="number"
+      scale={scale}
+      onChange={(e) => onTemperatureChange(e.target.value)}
+    />
+  );
+};
 
-    const ConvertTemperature = (temperature, to) => {
-        const input = parseFloat(temperature);
-            if (Number.isNaN(input)) {
-            return '';
-        }
-        return ( 
-            to === "f" ? (temperature * 9 / 5) + 32  : Math.round(((temperature - 32) * 5 / 9) * 1000) / 1000
-        );
-    }
+const convertTemperature = (temperature, targetUnit) => {
+  if (temperature === "") {
+    return (temperature = "");
+  }
+  if (targetUnit === "f") {
+    return Math.round(((temperature * 9) / 5 + 32) * 1000) / 1000;
+  }
+  if (targetUnit === "c") {
+    return Math.round((((temperature - 32) * 5) / 9) * 1000) / 1000;
+  }
+};
 
-    const TempZones = ({temperature}) => {
-        if (temperature >= 100) {
-            return <p className = "hot">🔥</p>;
-        }
-        else if (temperature < 0) {
-            return <p className = "cold">🥶</p>;
-        }
-        return <p className = "normal">💤</p>;
-      }
+const TempZone = ({ temperature }) => {
+  if (temperature >= 100) {
+    return <p className="hot">🔥</p>;
+  } else if (temperature < 0) {
+    return <p className="cold">🥶</p>;
+  } else {
+    return <p className="normal">💤</p>;
+  }
+};
 
-    const Calculator = () => {
+const Calculator = () => {
+  const [temperatureC, setTemperatureC] = useState("");
+  const [temperatureF, setTemperatureF] = useState("");
 
-        const [temperatureC, setTemperatureC] = useState("");
-        const [temperatureF, setTemperatureF] = useState("");
+  return (
+    <div className="container">
+      <div className="temp-celcius">
+        <p>Enter Temperature in Celsius:</p>
+        <TemperatureField
+          temperature={temperatureC}
+          scale="c"
+          onTemperatureChange={(t) => {
+            setTemperatureC(t);
+            setTemperatureF(convertTemperature(t, "f"));
+          }}
+        />
+      </div>
 
-    return(
-        <div className="container">
-
-            <div className="temp-celcius">
-            <p>Enter Temperature in Celsius:</p>
-            <TemperatureField 
-            temperature = {temperatureC} 
-            scale = "c" 
-            onTemperatureChange = {(t) => {
-                setTemperatureC(t);
-                setTemperatureF(ConvertTemperature(t, "f"));
-            }} 
-            />
-            </div>
-
-            <div className="temp-fahrenheit">
-            <p>Enter Temperature in Fahrenheit:</p>
-            <TemperatureField 
-            temperature = {temperatureF} 
-            scale = "f"  
-            onTemperatureChange = {(t) => {
-                setTemperatureF(t);
-                setTemperatureC(ConvertTemperature(t, "c"));
-            }} 
-            />
-            </div>
-
-            <div>
-                <TempZones 
-                temperature = {temperatureC}/>
-            </div>
-
-        </div>
-    );
-    
-}
+      <div className="temp-fahrenheit">
+        <p>Enter Temperature in Fahrenheit:</p>
+        <TemperatureField
+          temperature={temperatureF}
+          scale="f"
+          onTemperatureChange={(t) => {
+            setTemperatureF(t);
+            setTemperatureC(convertTemperature(t, "c"));
+          }}
+        />
+      </div>
+      <div>
+        <TempZone temperature={temperatureC} />
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
